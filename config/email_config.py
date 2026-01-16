d4614b395cfbb09946ed8819192785c653054c63
avinash-trisul <avinash.trisul@gmail.com>
2026-01-16 19:30:47 +0530

    fixed email

---
+++ config/email_config.py
@@ -0,0 +1,35 @@
+from typing import Dict
+from email.message import EmailMessage
+import smtplib
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+EMAIL = "gogulaavinash20@gmail.com"
+PASSWORD = "wsxa ooqq hvba hchn"
+
+
+def send_anomaly_email(to_email: str, anomaly: Dict):
+    subject = "🚨 Log Anomaly Detected"
+
+    body = f"""
+🚨 Anomaly Detected in System Logs
+
+Time Window : {anomaly['timestamp']}
+Error Count : {anomaly['error_count']}
+Z-Score     : {round(anomaly['z_score'], 2)}
+
+Please investigate immediately.
+
+Regards,
+Log Monitoring System
+"""
+
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = EMAIL
+    msg["To"] = to_email
+    msg.set_content(body)
+
+    with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
+        server.login(EMAIL, PASSWORD)
+        server.send_message(msg)
--- main.py
+++ main.py
@@ -1,24 +1,62 @@
 from config.dask_config import start_dask
 from processing.pipeline import build_pipeline
+from anomaly.detector import detect_anomaly
+from config.email_config import send_anomaly_email
 import time
+
+
+ADMIN_EMAIL = "admin@example.com" 
+
 
 def main():
     client = start_dask()
     print(client)
     print(f"Dashboard link: {client.dashboard_link}")
-    print("\n" + "="*50)
-    
+    print("\n" + "=" * 50)
+
     start = time.time()
+
+    # Build log processing pipeline
     log_df = build_pipeline("data/sample_log.log")
-    print("start time", start)
-    
+
     total_logs = log_df.count().compute()
     end = time.time()
-    
-    print("total logs parssed:", total_logs)
-    print("time taken", end)
-    
-    input("press enter....")
-    
+
+    print("Total logs parsed:", total_logs)
+    print("Time taken:", round(end - start, 2), "seconds")
+
+    print("\n Running anomaly detection...")
+
+    # Detect anomalies
+    anomalies_df = detect_anomaly(log_df)
+
+    # anomalies = anomalies_df.compute()
+    anomalies = anomalies_df
+
+    if anomalies.empty:
+        print("No anomalies detected")
+    else:
+        print(f"🚨 {len(anomalies)} anomalies detected!")
+
+        for _, row in anomalies.iterrows():
+            anomaly_data = {
+                "timestamp": row["timestamp"],
+                "error_count": row["error_count"],
+                "z_score": row["z_score"]
+            }
+
+            send_anomaly_email(
+                to_email=ADMIN_EMAIL,
+                anomaly=anomaly_data
+            )
+
+            print(
+                f"📧 Alert sent | Time: {row['timestamp']} | "
+                f"Errors: {row['error_count']}"
+            )
+
+    input("\nPress Enter to exit...")
+
+
 if __name__ == "__main__":
     main()
